5464802cf6931eae51911980c4b4b853e3ecbee8
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2019-07-04 14:27:58 +0800

    构建server初始版本

--- proxy-server/src/main/java/com/noisy/flappy/server/ProxyServerContainer.java
+++ proxy-server/src/main/java/com/noisy/flappy/server/ProxyServerContainer.java
@@ -14,6 +14,7 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import java.net.BindException;
 import java.util.List;
@@ -24,6 +25,7 @@ import java.util.List;
  * @date 2019/6/28
  */
 @Slf4j
+@Component
 public class ProxyServerContainer implements Container, ProxyConfig.ConfigChangedListener {
 
     /**
