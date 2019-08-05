85ae4bf2f1a4e5c1f68e2d64082bada12b4dcc95
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2019-08-05 15:59:58 +0800

    update

---
+++ proxy-common/src/main/java/com/noisy/flappy/common/CommonMain.java
@@ -0,0 +1,12 @@
+package com.noisy.flappy.common;
+
+/**
+ * @author lei.X
+ * @date 2019/8/5
+ */
+public class CommonMain {
+
+    public static void main(String[] args) {
+        System.out.println("Just for compile");
+    }
+}
---
+++ proxy-protocol/src/main/java/com/noisy/flappy/protocol/ProtocolMain.java
@@ -0,0 +1,13 @@
+package com.noisy.flappy.protocol;
+
+/**
+ * @author lei.X
+ * @date 2019/8/5
+ */
+public class ProtocolMain {
+
+
+    public static void main(String[] args) {
+        System.out.println("just for compile");
+    }
+}
