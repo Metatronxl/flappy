b34be63ac9ec5c56ce6fab45dd51eaca88092daf
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2019-08-28 15:34:03 +0800

    Vue login page finished

---
+++ proxy-server/src/main/java/com/noisy/flappy/server/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.noisy.flappy.server.controller;
+
+/**
+ * @author lei.X
+ * @date 2019/8/28
+ */
+public class LoginController {
+}
---
+++ proxy-server/src/main/java/com/noisy/flappy/server/entity/Result.java
@@ -0,0 +1,8 @@
+package com.noisy.flappy.server.entity;
+
+/**
+ * @author lei.X
+ * @date 2019/8/28
+ */
+public class Result {
+}
